b109f15e1f2fd30ee8f68b78e6119f11734e64d4
?ngel ?lvarez <angel@nan-tic.com>
2016-12-29 17:00:03 +0100

    use field from project_revenue

--- purchase.py
+++ purchase.py
@@ -15,7 +15,7 @@ class Work:
     @classmethod
     def _get_related_cost_and_revenue(cls):
         res = super(Work, cls)._get_related_cost_and_revenue()
-        return res + [('purchase.line', 'parent', '_get_revenue',
+        return res + [('purchase.line', 'work', '_get_revenue',
             '_get_cost')]
 
 
@@ -24,30 +24,24 @@ class ProjectSummary:
     __name__ = 'project.work.summary'
     __metaclass__ = PoolMeta
 
-    # @classmethod
-    # def union_models(cls):
-    #     res = super(ProjectSummary, cls).union_models()
-    #     return ['purchase.line'] + res
+    @classmethod
+    def union_models(cls):
+        res = super(ProjectSummary, cls).union_models()
+        return ['purchase.line'] + res
 
-
-
-#TODO: project revenue uses work field, but until unionmixing works correctly
-#      i create a new field.
 
 class PurchaseLine:
     __metaclass__ = PoolMeta
     __name__ = 'purchase.line'
 
-    parent = fields.Many2One('project.work', 'Work Effort', select=True,
-        domain=[
-            ('company', '=', Eval('_parent_purchase', {}).get('company', -1)),
-            ])
-
-
     @classmethod
     def _get_cost(cls, purchase_lines):
-        return dict( (w.id, w.amount) for w in purchase_lines)
+        return dict((w.id, w.amount) for w in purchase_lines)
 
     @classmethod
     def _get_revenue(cls, purchase_lines):
         return dict.fromkeys([w.id for w in purchase_lines], Decimal(0))
+
+    @staticmethod
+    def _get_summary_related_field():
+        return 'work'
